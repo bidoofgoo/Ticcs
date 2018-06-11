796069cdb0c1e42294ea51bfc077068c27633238
Joshuaneuro <joshuavdmeij@gmail.com>
2018-06-11 13:55:02 +0200

    updated javascript
    added more functions, and making it easier to add panelfunction effects

--- public/js/script.js
+++ public/js/script.js
@@ -1,45 +1,59 @@
 window.onload = function() {
+  console.log("JavaScript loaded");
   // p stands for the the panel en c for the color
   var panel1 = document.getElementById("rightrow1Col1");
   var panel2 = document.getElementById("rightrow1Col2");
   var panel3 = document.getElementById("rightrow2Col1");
   var panel4 = document.getElementById("rightrow2Col2");
 
-  console.log("JavaScript loaded");
-
   var color1 = "white";
   var color2 = "red";
   var color3 = "green";
   var color4 = "blue";
 
+  var var1 = false;
+  var var2 = false;
+  var var3 = false;
+  var var4 = false;
+  var var5 = false;
+  var var6 = false;
+
+  // iniating functions, if you wanna add more its easy to do here
   panel1.onclick = function(){
-    changePanel1();
+    changeVar(var1, panel1);
   };
   panel2.onclick = function(){
-    changePanel2();
+    changeVar(var2, panel2);
   };
   panel3.onclick = function(){
-    changePanel3();
+    changeVar(var3, panel3);
   };
   panel4.onclick = function(){
-    changePanel4();
+    changeVar(var4, panel4);
   };
-
+  // reuseable funtions
+  // must give a panel and color with the function $panel for which panel you want, $color for which color.
   function changeColor(panel, color) {
     panel.style.backgroundColor = color;
   }
+  // var for the personal boolean for the panel, and which panel;
+  function changeVar(var, panel) {
+    if (!var) {
+      var = true;
+      changeColor(panel,color3);
+      // you can change the value here so it returns true or false for you forms
+      console.log("false");
+    }
+    else {
+      var = false;
+      changeColor(panel,color2);
+      console.log("true");
+    }
+  }
 
-  function changePanel1() {
-    console.log("function 1 loaded");
-    changeColor(panel1, color1);
+  // get x
+  function getElement(x) {
+    var x = document.getElementById(x);
   }
-  function changePanel2() {
-    changeColor(panel2, color2);
-  }
-  function changePanel3() {
-    changeColor(panel3, color3);
-  }
-  function changePanel4() {
-    changeColor(panel4, color4);
-  }
+  console.log("scrip ended succesfully");
 }
