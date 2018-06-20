cc7dcb8dfd21e045e1234a90d6416e220699013d
Joshuaneuro <joshuavdmeij@gmail.com>
2018-06-20 11:12:32 +0200

    Changes javascript for new version
    needs testing

--- resources/views/vragenlijst/vragenlijst.blade.php
+++ resources/views/vragenlijst/vragenlijst.blade.php
@@ -12,6 +12,7 @@
    <link href="{{ asset('public/css/ticchome.css') }}" rel="stylesheet">
    <link href="{{ asset('public/css/ticc.css') }}" rel="stylesheet">
    <link href="{{ asset('public/css/vragen2.css') }}" rel="stylesheet">
+   <script type="text/javascript" src="{{ URL::asset('js/script.js') }}"></script>
 
    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
    <script type="text/javascript" src="color.js"></script>
