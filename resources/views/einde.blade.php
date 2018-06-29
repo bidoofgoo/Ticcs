b0d446488c664b85081739d20af4528645492714
bidoofgoo <bidoofgoo@users.noreply.github.com>
2018-06-29 11:49:04 +0200

    kleine tekst aanpassing

--- resources/views/einde.blade.php
+++ resources/views/einde.blade.php
@@ -8,7 +8,7 @@
 @section('content')
 <main class="flex flexcenter flexmiddle fillings">
   <div class="boop">
-    <h1 >De vragenlijst is geopend op een nieuwe pagina.</h1>
+    <h1 >De resultaten zijn geopend in een nieuw tabblad.</h1>
     <h1 style="margin-bottom: 2rem;">Wij hopen dat we ondersteuning hebben kunnen bieden bij uw problemen.</h1>
     <a class="button" href="{{url('vragenlijst/1')}}" style="font-size: 2rem; padding: 1rem; width: 20rem;"> Doe de test opnieuw </a>
   </div>
